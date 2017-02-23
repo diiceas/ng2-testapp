8ac049ea44379f7b53772dadddebe6d7ac93e4b9
diiceas <forthepost@gmail.com>
2017-02-23 03:18:36 +0100

    set up routing - intermediate changes

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -1,18 +1,32 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
 import { Hero } from './hero'
+import 'rxjs/add/operator/switchMap';
+
+import { HeroService } from './hero.service'
 
 @Component({
-    selector: 'my-hero-detail',
-    template: `<div *ngIf="hero">
-    <h1>{{title}}</h1>
-    <h2>{{hero.name}} details!</h2>
-    <div><label>id: </label>{{hero.id}}</div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="hero.name" placeholder="name">
-    </div>
-  </div>`
+  moduleId: module.id,
+  selector: 'my-hero-detail',
+  templateUrl: './hero-detail.component.html' 
 })
-export class HeroDetailComponent{
-   @Input() hero: Hero;    
+export class HeroDetailComponent implements OnInit {
+  @Input() hero: Hero;
+
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) { }
+
+  ngOnInit(): void {
+    this.route.params
+      .switchMap((params: Params) => this.heroService.getHero(+params['id']))
+      .subscribe(hero => this.hero = hero)
+  }
+
+  goBack(){
+    this.location.back();
+  }
 }
--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -2,5 +2,10 @@ import { Hero } from './hero'
 
 export const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' }
+  { id: 13, name: 'Batman' },
+  { id: 14, name: 'Super man' },
+  { id: 15, name: 'Capitan America' },
+  { id: 16, name: 'Cat Woman' },
+  { id: 17, name: 'Black Widow' },
+  { id: 18, name: 'Spider Man' }
 ];
